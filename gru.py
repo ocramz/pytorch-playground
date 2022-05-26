0d7e9134b6b54091590d2f4e3858206207661f1f
Marco Zocca <marco.zocca@unfoldml.com>
2022-05-26 10:38:52 +0200

    gru impl

--- gru.py
+++ gru.py
@@ -1,7 +1,7 @@
-from torch import Tensor, tensor, zeros, ones, randn, diag, cuda
+from torch import Tensor, tensor, zeros, ones, randn, diag, cuda, _assert, Size
 from torch.nn import Module, Sequential, Linear, Sigmoid
 from torch.linalg import matmul
-from torch.nn.functional import linear, sigmoid
+from torch.nn.functional import linear, sigmoid, tanh
 from torch.utils.data import DataLoader
 from torchtext.vocab import Vocab, build_vocab_from_iterator
 import io
@@ -10,33 +10,51 @@ import io
 device = "cuda" if cuda.is_available() else "cpu"
 print(f"Using {device}")
 
+
+def assertIsVector(x: Tensor):
+    _assert(len(x.size()) == 1, f'{x} should be a vector')
+
+
 def hadamard(a, b):
-    """Hadamard product"""
+    """Hadamard (componentwise) product of two vectors"""
+    _assert(a.size() == b.size(), 'arguments should have the same size')
+    assertIsVector(a)
+    assertIsVector(b)
     return linear(a, diag(b))
 
+
 class GRU(Module):
-    def __init__(self, nh):
+    def __init__(self, nh:int, d:int):
         super(GRU, self).__init__()
-        self.h = randn(nh)  # initial state
-        self.hprev = randn(nh)
-        self.zt = randn(nh)
-        self.Wz = Linear(nh, nh, bias=False)
-        self.Uz = Linear(nh, nh, bias=True)
-    def forward(self, xs):
+        self.htilde = randn(nh)  # candidate state
+        self.hprev = randn(nh)  # h_{t-1} is random at time 0
+        self.h = randn(nh)  # h_{t} is random at time 0
+        self.zt = randn(nh)  # update gate
+        self.rt = randn(nh)  # reset gate
+        self.Wh = Linear(nh, d, bias=False)
+        self.Wz = Linear(nh, d, bias=False)
+        self.Wr = Linear(nh, d, bias=False)
+        self.Uh = Linear(nh, d, bias=False)
+        self.Uz = Linear(nh, d, bias=True)
+        self.Ur = Linear(nh, d, bias=True)
+
+    def forward(o, xs):
         for i, x in enumerate(xs):
-            if i == 0:
-                self.h = hadamard(1 - self.zt, self.hprev) + hadamard(self.zt, self.h)
-                self.hprev = self.h  # update h_(t-1)
-                self.zt = sigmoid()
-            else:
-                pass
-        pass
+            o.h = hadamard(1 - o.zt, o.hprev) + hadamard(o.zt, o.htilde)
+            o.zt = sigmoid(matmul(o.Wz, x) + matmul(o.Uz, o.hprev))
+            o.htilde = tanh(matmul(o.Wh, x) + hadamard(o.rt, matmul(o.Uh, o.hprev)))
+            o.rt = sigmoid(matmul(o.Wr, x) + matmul(o.Ur, o.hprev))
+            o.hprev = o.h  # update h_(t-1)
+
+model = GRU(5, 10).to(device)
+print(model)
 
 
 def yield_tokens(file_path, sep=' '):
-     with io.open(file_path, encoding = 'utf-8', mode='r') as f:
-         for line in f:
-             yield line.strip(sep).split(sep)
+    with io.open(file_path, encoding='utf-8', mode='r') as f:
+        for line in f:
+            yield line.strip(sep).split(sep)
+
 
 def embedString(voc, s, sep=' '):
     """tokenize the string and look up the tokens in the Vocab (dictionary) object"""
@@ -44,15 +62,17 @@ def embedString(voc, s, sep=' '):
     ils = voc.lookup_indices(iis)
     return tensor(ils)
 
-def mkVocab(file_path) :
+
+def mkVocab(file_path):
     """build a Vocab out of a text file"""
     v = build_vocab_from_iterator(yield_tokens(file_path),
-                                     specials=["<unk>"])
+                                  specials=["<unk>"])
     v.set_default_index(0)
     return v
 
+
 if __name__ == '__main__':
-    fpath = 'data/text0'
+    fpath = 'data/alice'
     voc = mkVocab(fpath)
     print(voc.lookup_token(0))
     print(embedString(voc, 'Alice took a xyz'))
