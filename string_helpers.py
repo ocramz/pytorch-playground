8b40175a765a9461678c2090569e3b0752701eb9
Marco Zocca <marco.zocca@unfoldml.com>
2022-05-27 00:23:52 +0200

    Dataset interface

--- string_helpers.py
+++ string_helpers.py
@@ -1,10 +1,13 @@
 from torch import Tensor, tensor, zeros, ones, randn, diag, cuda, Size, from_numpy
+from torch.utils.data import Dataset, DataLoader
 import numpy as np
 # from torchtext.data import get_tokenizer
 from torchtext.vocab import Vocab, vocab, build_vocab_from_iterator
 import io
+from os import path
 import re
 from itertools import chain
+from linecache import getline
 from collections import Counter, OrderedDict
 from bimap import BiMap
 
@@ -32,14 +35,9 @@ class Tokenize:
         :returns iterator of string n-grams
         :rtype list of str"""
         toks = self.split(s)
-        # print(toks)
         n = self.ngramSize
-        # ngit = chain.from_iterable(ngrams(t, n) for t in toks)  # iterable
-        # ngrs = list(ngit)
-        ngrs = []
-        for t in toks:
-            # print(t, ngrs)
-            ngrs = ngrs + ngrams(t, n)
+        ngit = chain.from_iterable(ngrams(t, n) for t in toks)  # iterable
+        ngrs = list(ngit)
         return ngrs
     def ngramCounts(self, s:str):
         ngrs = self.ngrams(s)
@@ -52,14 +50,92 @@ def t0():
     the book her sister was reading, but it had no pictures or
     conversations in it, “and what is the use of a book,” thought Alice
     “without pictures or conversations?”
+    So she was considering in her own mind (as well as she could, for the
+hot day made her feel very sleepy and stupid), whether the pleasure of
+making a daisy-chain would be worth the trouble of getting up and
+picking the daisies, when suddenly a White Rabbit with pink eyes ran
+close by her.
+There was nothing so _very_ remarkable in that; nor did Alice think it
+so _very_ much out of the way to hear the Rabbit say to itself, “Oh
+dear! Oh dear! I shall be late!” (when she thought it over afterwards,
+it occurred to her that she ought to have wondered at this, but at the
+time it all seemed quite natural); but when the Rabbit actually _took a
+watch out of its waistcoat-pocket_, and looked at it, and then hurried
+on, Alice started to her feet, for it flashed across her mind that she
+had never before seen a rabbit with either a waistcoat-pocket, or a
+watch to take out of it, and burning with curiosity, she ran across the
+field after it, and fortunately was just in time to see it pop down a
+large rabbit-hole under the hedge.
     """
-    tok = Tokenize()
+    tok = Tokenize(ngramSize=2)
     ngrs = tok.ngrams(s)
     voc = BiMap(ngrs)
-    # print(voc)
     testStr = 'books are nice and not boring'
-    # print(tok.ngrams(testStr))
-    return embedStringBM(voc, 'books are nice and not boring', tok)
+    return embedStringBM(voc, testStr, tok)
+
+class TextDataset(Dataset):
+    def __init__(self, fpath, xdim, tok:Tokenize = Tokenize()):
+        """
+        :param fpath: path of text data file
+        :param xdim: vector dimension
+        :param tok:  tokenizer
+        """
+        self.fpath = fpath
+        self.tok = tok
+        self.xdim = xdim
+        self.voc = ngramsFromTextFile(fpath, tok)
+    def __len__(self):
+        _, n = fileBounds(self.fpath)
+        return n
+    def __getitem__(self, ix):
+        s = stringAtIx(self.fpath, ix)
+        print(s)  # debug
+        x, y = embedStringBM(self.voc, s, self.tok, self.xdim)
+        return x, y
+
+
+
+def fileBounds(fpath):
+    with open(fpath) as f:
+        numLines = len(f.readlines())
+        fsizeBytes = path.getsize(fpath)
+        return numLines, fsizeBytes
+
+def stringAtIx(fpath, ix, k=10):
+    # with open(fpath) as f:
+    #     numLines = len(f.readlines())
+    #     fsizeBytes = path.getsize(fpath)
+    numLines, fsizeBytes = fileBounds(fpath)
+    bytesPerLine = fsizeBytes // numLines
+    m = ix // numLines + 1
+    n = ix % bytesPerLine
+    return stringFromTextFile(fpath, m, n, k)
+
+
+def stringFromTextFile(fpath, m, n, k):
+    """load a text string of given length from a given row, colum of a file
+    :param m: line #
+    :param n: column #
+    :param k: string max length
+    """
+    txt = getline(fpath, m)[n:]
+    ll = len(txt)
+    if ll < k:
+        return txt
+    elif ll > k:
+        return txt[0:k]
+    else:
+        return txt
+
+def ngramsFromTextFile(fpath, tok:Tokenize = Tokenize()):
+    """build a vocabulary of n-grams from a text file
+    :returns BiMap of text n-grams"""
+    bm = BiMap()
+    with io.open(fpath, encoding='utf-8', mode='r') as f:
+        for line in f:
+            ngrs = tok.ngrams(line)
+            bm.fromIter(ngrs)
+        return bm
 
 
 
@@ -71,12 +147,23 @@ def yield_tokens(file_path, tok: Tokenize):
             if toksClean:
                 yield toksClean
 
-def embedStringBM(voc: BiMap, s:str, tok:Tokenize):
-    """:returns a Tensor"""
+def embedStringBM(voc: BiMap, s:str, tok:Tokenize, dim0: int=10):
+    """:returns a Tensor of given dimension and label. Pads missing elements or removes extra ones"""
     iis = tok.ngrams(s)
     ils = voc.lookupVDs(iis)
-    # print(list(ils))  # debug
-    return from_numpy(np.fromiter(ils, dtype=int))
+    z = voc.defaultIx
+    ilsl = list(ils)
+    d = len(ilsl)  # length of n-gram list
+    dim = dim0 + 1
+    if d == dim:
+        ilsPrep = ilsl
+    elif d < dim:
+        ilsPrep = ilsl + ([z] * (dim - d))
+    else:
+        ilsPrep = ilsl[0:dim]
+    x = from_numpy(np.fromiter(ilsPrep[0:dim0], dtype=int))
+    y = ilsPrep[-1]  # label is the _last_ element
+    return x, y
 
 def embedString(voc: Vocab, s: str, tok: Tokenize):
     """tokenize the string and look up the tokens in the Vocab (dictionary) object"""
