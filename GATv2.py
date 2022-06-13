2474e34c4c6fa7849aec7448ca9e54d64d0ef6fa
Marco Zocca <marco.zocca@unfoldml.com>
2022-06-13 11:32:37 +0200

    Graph interface

--- GATv2.py
+++ GATv2.py
@@ -28,38 +28,94 @@ class GATv2(Module):
         scoreMtx = sparse_coo_tensor([iis, jjs], vvs)
         attn = softmax(scoreMtx, dim=1)  # attention weights alpha_i,j
 
-
 class Graph:
     def __init__(self):
-        self.d = {}
-    def __iter__(self):
-        for e in self.d.items():
-            yield e
+        self.vertices = {}
+        self.edges = {}
     def __repr__(self):
-        return f'{str(self.d)}'
-    def tuples(self):
-        for (i, j), v in self.d.items():
-            yield i, j, v
-    def insert(self, ij: (int, int), v):
-        i, j = ij
-        self.d[(i, j)] = v
-    def neighbors(self, refIx:int, transpose=False):
+        return f'Nodes : {str(self.vertices)}, edges : {str(self.edges)}'
+    def lookupNode(self, k):
+        try:
+            v0 = self.vertices[k]
+            return v0
+        except KeyError as e:
+            return None
+    def lookupEdge(self, i):
+        try:
+            i2 = self.edges[i]
+            return i2
+        except KeyError as e:
+            return None
+    def node(self, i, vv):
+        if self.lookupNode(i) is None:
+            self.vertices[i] = vv
+    def edge(self, i1, i2):
+        """add an edge"""
+        i2m = self.lookupEdge(i1)
+        if i2m is None:
+            self.edges[i1] = [i2]
+        else:
+            self.edges[i1] = [i2] + i2m
+    def neighbors(self, refIx: int, transpose=False):
         nn = []
-        for i, j in self.d.keys():
-            if transpose:
+        print(str(self.edges.values()))
+        print(str(self.edges.items()))
+        if transpose:
+            for j in self.edges.values():
                 if j == refIx:
-                    nn.append(i)
-            else:
-                if i == refIx:
                     nn.append(j)
+        else:
+            for i in self.edges.items():
+                if i == refIx:
+                    nn.append(i)
         return nn
-
 
 def graphFromList(ll):
     g = Graph()
-    for k, v in ll:
-        #print(f'{k}, {v}')
-        g.insert(k, v)
+    for i1, i2, v1, v2 in ll:
+        g.node(i1, v1)
+        g.node(i2, v2)
+        g.edge(i1, i2)
     return g
+
+
+        # if self.lookupNode(i1) is not None and self.lookupEdge(i2) is not None:
+        #     en = self.lookupEdge(i1)
+        #     if en is None or not(en == i1):
+        #         self.e[i1] = i2
+
+
+
+# class Graph:
+#     def __init__(self):
+#         self.d = {}
+#     def __iter__(self):
+#         for e in self.d.items():
+#             yield e
+#     def __repr__(self):
+#         return f'{str(self.d)}'
+#     def tuples(self):
+#         for (i, j), v in self.d.items():
+#             yield i, j, v
+#     def insert(self, ij: (int, int), v):
+#         i, j = ij
+#         self.d[(i, j)] = v
+#     def neighbors(self, refIx:int, transpose=False):
+#         nn = []
+#         for i, j in self.d.keys():
+#             if transpose:
+#                 if j == refIx:
+#                     nn.append(i)
+#             else:
+#                 if i == refIx:
+#                     nn.append(j)
+#         return nn
+#
+# def graphFromList(ll):
+#     g = Graph()
+#     for k, v in ll:
+#         #print(f'{k}, {v}')
+#         g.insert(k, v)
+#     return g
 
 
